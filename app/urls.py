6b4cf67849659e0e4772e729fb7c2c40ee52b9e0
Joao Vitor Rodrigues <jvor.rodrigues@gmail.com>
2022-03-31 20:31:32 -0300

    march31

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('painel/', views.dashboardPanel, name='panel'),
     path('lista/', views.objectList, name='list'),
+    path('sobre/', views.about, name='about'),
+    path('contribue/', views.contribute, name='contribute'),
     path('objeto/<int:tessID>/', views.tessObject, name='object')
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from matplotlib.font_manager import json_dump, json_load
+import pandas as pd
+import json
 
 def index(request):
-    return render(request, 'app\index.html')
+    return render(request, 'app/home.html')
 
 def dashboardPanel(request):
-    return HttpResponse('Painel de Indicadores')
+    return render(request, 'app/dashboard.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def contribute(request):
+    return render(request, 'app/contribute.html')
 
 def objectList(request):
-    return HttpResponse('Lista de Objetos')
+    df = pd.read_csv('exofop_tess_toisv1.csv', sep=';')
+    jsonList = df.reset_index().to_json(orient='split')
+    data = []
+    data = json.loads(jsonList)
+    context = {'d' : data}
+
+    return render(request,'app/objectlist.html', context)
 
 def tessObject(request, tessID):
-    return HttpResponse('Objeto %s.' % tessID)
+    return render(request, 'app/object.html', tessID)
