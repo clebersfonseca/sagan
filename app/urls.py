df4c0e5b49a2bd0befb9e51db80b6da55fd90249
Joao Vitor Rodrigues <jvor.rodrigues@gmail.com>
2022-04-26 20:11:05 -0300

    april26

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('painel/', views.dashboardPanel, name='panel'),
     path('lista/', views.objectList, name='list'),
     path('sobre/', views.about, name='about'),
     path('contribue/', views.contribute, name='contribute'),
     path('lista/observacoes', views.returnModal, name='modal'),
-    path('objeto/<int:ticID>/', views.tessObject, name='object')
+    path('lista/observacoes2', views.modal, name='modal2'),
+    path('objeto/<int:ticID>/<int:ind>', views.tessObject, name='object'),
+    path('objetojson/<int:ticID>/<int:ind>', views.returnJson, name='retornoJ')
 ]
--- app/views.py
+++ app/views.py
@@ -10,9 +10,6 @@ from django.http import JsonResponse
 def index(request):
     return render(request, 'app/home.html')
 
-def dashboardPanel(request):
-    return render(request, 'app/dashboard.html')
-
 def about(request):
     return render(request, 'app/about.html')
 
@@ -28,6 +25,23 @@ def objectList(request):
 
     return render(request,'app/objectlist.html', context)
 
+def modal(request):
+    ticID = request.POST['id']
+    idParam = 'TIC ' + ticID
+    search_result = lk.search_lightcurve(idParam, mission='TESS')
+    result = str(search_result)
+
+    df = pd.read_json(result)
+    df.rename(columns={"year" : "ano"})
+
+    jsonData = df.reset_index().to_json(orient="records")
+    data = []
+    data = json.loads(jsonData)
+
+    context = {'d' : data}
+
+    return render(request, 'app/modal.html', context)
+
 def returnModal(request):
     ticID = request.POST['id']
     idParam = 'TIC ' + ticID
@@ -38,5 +52,22 @@ def returnModal(request):
 
     return HttpResponse(df.to_json())
 
-def tessObject(request, ticID, indexList):
-    return render(request, 'app/object.html', ticID)
+def tessObject(request, ticID, ind):
+    
+    context = {'ticID' : ticID, 'ind' : ind}
+
+    return render(request, 'app/object.html', context)
+
+def returnJson(request, ticID, ind):
+    ticID = ticID
+    obsIndex = ind
+
+    idParam = 'TIC ' + str(ticID)
+    search_result = lk.search_lightcurve(idParam, mission='TESS')
+
+    lc = search_result[obsIndex].download()
+
+    df = pd.DataFrame(lc.time.value, columns=["time"])
+    df["data"] = lc.flux.value
+
+    return JsonResponse(df.to_json(), safe=False)
